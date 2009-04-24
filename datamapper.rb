a706b34bbf1b0e6115de4150aa0e7a1358771c22
Jeff Larkin <larkin@larkin-laptop.(none)>
2009-04-23 21:06:20 -0400

    Made it possible to choose which do_* package to install.  Added dm-types.

--- datamapper.rb
+++ datamapper.rb
@@ -1,11 +1,15 @@
 # inspired by http://www.rowtheboat.com/archives/32
+# slightly modified by jefflarkin
 
 
 # get all datamapper related gems (assume sqlite3 to be database)
 gem "addressable", :lib => "addressable/uri"
-gem "do_sqlite3"
+gem "do_sqlite3" if yes?("Install do_sqlite3?")
+gem "do_mysql" if yes?("Install do_mysql?")
+gem "do_postgres" if yes?("Install do_postgres?")
 gem 'dm-validations'
 gem 'dm-timestamps'
+gem "dm-types"
 gem "rspec", :lib => false
 gem "rspec-rails", :lib => false
 gem "datamapper4rail", :lib => 'datamapper4rails' # excuse the typo
